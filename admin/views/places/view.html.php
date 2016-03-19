8ac8244268c9069a81f7a0b3dfd8d10ee7f61bc0
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-03-19 15:46:20 -0400

    still debugging getModel() from view

--- admin/views/places/view.html.php
+++ admin/views/places/view.html.php
@@ -36,7 +36,7 @@ class PvpollingplacesViewPlaces extends JView
         $t->appendButton('Link', 'default', 'Export Filter', 'index.php?option=com_pvpollingplaces&controller=places&format=raw');
         // Get data from the model
 
-        d($this, $this->getModel('Wards'));
+        d($this->getModel('Wards'), $this);
         $items = &$this->get('Data');
         $pagination = &$this->get('Pagination');
 
