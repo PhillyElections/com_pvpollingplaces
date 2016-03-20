fc57760f8901ac672b3c1e0ae49e72519257e61a
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-03-20 16:44:54 -0400

    new combo submit wiring

--- admin/assets/js/places.js
+++ admin/assets/js/places.js
@@ -88,5 +88,5 @@ var AC = (function() {
   return outer;
 })();
 window.addEvent('domready', function() {
-  document.querySelectAll('data-action=submits').addListener('change', function() {form.submit();});
+  document.querySelectAll('#ward,#d_id').addListener('change', function() {document.getElementById('adminForm').submit();});
 });
