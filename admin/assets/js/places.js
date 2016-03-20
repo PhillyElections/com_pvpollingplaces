88c9fb65945ded36ad51d01925286a6623c07ba3
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-03-20 17:59:40 -0400

    correcting event code

--- admin/assets/js/places.js
+++ admin/assets/js/places.js
@@ -2,6 +2,6 @@ window.addEvent('domready', function() {
   var combos = document.querySelectorAll('[data-action="submits"]');
   for (var i=0; i<combos.length; i++) {
     console.log(combos[i]);
-    combos[i].addEventListener('onchange', function() {document.getElementById('adminForm').submit();});    
+    combos[i].addEventListener('change', function() {document.getElementById('adminForm').submit();});    
   }
 });
