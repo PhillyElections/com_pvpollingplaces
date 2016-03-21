159005641801b019e5a089a6442280194586256d
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-03-21 16:19:39 -0400

    adding debug dump

--- admin/views/places/tmpl/default.php
+++ admin/views/places/tmpl/default.php
@@ -2,7 +2,7 @@
 $pagination = &$this->pagination;
 
 jimport("pvcombo.PVCombo");
-
+d(JRequest::get('REQUEST'));
 $document = &JFactory::getDocument();
 $document->addCustomTag('<script src="components/com_pvpollingplaces/assets/js/places.js" async defer></script>');
 ?>
@@ -26,7 +26,7 @@ $document->addCustomTag('<script src="components/com_pvpollingplaces/assets/js/p
 <input type="checkbox" value="11" <?=JRequest::getVar('ward', false) && in_array('11', JRequest::getVar('ward')) ? 'checked' : '';?> \>
 <input type="checkbox" value="12" <?=JRequest::getVar('ward', false) && in_array('12', JRequest::getVar('ward')) ? 'checked' : '';?> \>
 
-                    <?php //=JHTML::_('select.genericlist', PVCombo::getsFromObject($this->wards, 'ward', 'ward'), 'ward', 'multiple data-action="submits"', 'idx', 'value', (JRequest::getVar('ward') ? JRequest::getVar('ward') : ''), 'ward');;;?>
+                    <?php //=JHTML::_('select.genericlist', PVCombo::getsFromObject($this->wards, 'ward', 'ward'), 'ward', 'multiple data-action="submits"', 'idx', 'value', (JRequest::getVar('ward') ? JRequest::getVar('ward') : ''), 'ward');;;;?>
                     </th>
                     <th><?=JText::_('Division');?>
                     <?=(isset($this->divisions) && count($this->divisions) ? JHTML::_('select.genericlist', PVCombo::getsFromObject($this->divisions, 'division', 'division'), 'd_id', 'multiple data-action="submits"', 'idx', 'value', (JRequest::getVar('d_id') ? JRequest::getVar('d_id') : ''), 'd_id') : JText::_('Division'));?></th>
