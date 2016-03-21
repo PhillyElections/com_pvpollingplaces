249312df95a1127e192cf3ccfe058bbb4f085545
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-03-21 08:34:00 -0400

    wiring for blur rather than change

--- admin/assets/js/places.js
+++ admin/assets/js/places.js
@@ -1,6 +1,6 @@
 window.addEvent('domready', function() {
   var combos = document.querySelectorAll('[data-action="submits"]');
   for (var i=0; i<combos.length; i++) {
-    combos[i].addEventListener('change', function() {document.getElementById('adminForm').submit();});    
+    combos[i].addEventListener('blur', function() {document.getElementById('adminForm').submit();});    
   }
 });
