9c7001cbc939c4b9ab04b02407312483c72a4a45
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-03-22 10:30:36 -0400

    Can we load bootstrap without destroying the universe?

--- admin/views/places/tmpl/default.php
+++ admin/views/places/tmpl/default.php
@@ -4,7 +4,10 @@ $pagination = &$this->pagination;
 jimport("pvcombo.PVCombo");
 d(JRequest::get('REQUEST'));
 $document = &JFactory::getDocument();
-//$document->addCustomTag('<script src="components/com_pvpollingplaces/assets/js/places.js" async defer></script>');
+$document->addCustomTag('<script src="components/com_pvpollingplaces/assets/js/places.js" async defer></script>');
+$document->addCustomTag('<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js" async defer></script>');
+$document->addCustomTag('<script src="/public_html/media/bootstrap/dist/js/bootstrap.min.js" async defer></script>');
+//$document->addCustomTag('<script src="/public_html/media/bootstrap/dist/js/bootstrap.min.js" async defer></script>');
 ?>
 <form action="<?=JRoute::_('index.php?option=com_pvpollingplaces');?>" method="post" name="adminForm" id="adminForm">
     <div id="editcell">
