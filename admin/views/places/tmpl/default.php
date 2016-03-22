0324fa2aba22c62d1301b3affc04c4da7b24fb6f
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-03-21 21:26:46 -0400

    starting to separate out filter

--- admin/views/places/tmpl/default.php
+++ admin/views/places/tmpl/default.php
@@ -11,6 +11,16 @@ $document->addCustomTag('<script src="components/com_pvpollingplaces/assets/js/p
         <table class="adminlist">
             <thead>
                 <tr>
+                    <th colspan="14">
+<?php
+for ($i = 0; $i < count($this->wards); $i++):
+?><label><?=JText::_('Ward');?><input name="ward[]" type="checkbox" value="<?=$i;?>" <?=JRequest::getVar('ward', false) && in_array($i, JRequest::getVar('ward')) ? 'checked' : '';?> \></label>
+<?php
+endfor;
+?>
+                    </th>
+                </tr>
+                <tr>
                     <th width="5px">
                         <?=JText::_('ID');?>
                     </th>
@@ -21,12 +31,7 @@ $document->addCustomTag('<script src="components/com_pvpollingplaces/assets/js/p
                         P
                     </th>
                     <th><?=JText::_('Ward');?>
-<input name="ward[]" type="checkbox" value="1" <?=JRequest::getVar('ward', false) && in_array('1', JRequest::getVar('ward')) ? 'checked' : '';?> \>
-<input name="ward[]" type="checkbox" value="2" <?=JRequest::getVar('ward', false) && in_array('2', JRequest::getVar('ward')) ? 'checked' : '';?> \>
-<input name="ward[]" type="checkbox" value="11" <?=JRequest::getVar('ward', false) && in_array('11', JRequest::getVar('ward')) ? 'checked' : '';?> \>
-<input name="ward[]" type="checkbox" value="12" <?=JRequest::getVar('ward', false) && in_array('12', JRequest::getVar('ward')) ? 'checked' : '';?> \>
-
-                    <?php //=JHTML::_('select.genericlist', PVCombo::getsFromObject($this->wards, 'ward', 'ward'), 'ward', 'multiple data-action="submits"', 'idx', 'value', (JRequest::getVar('ward') ? JRequest::getVar('ward') : ''), 'ward');;;;;;?>
+                    <?php //=JHTML::_('select.genericlist', PVCombo::getsFromObject($this->wards, 'ward', 'ward'), 'ward', 'multiple data-action="submits"', 'idx', 'value', (JRequest::getVar('ward') ? JRequest::getVar('ward') : ''), 'ward');;;;;;;?>
                     </th>
                     <th><?=JText::_('Division');?>
                     <?=(isset($this->divisions) && count($this->divisions) ? JHTML::_('select.genericlist', PVCombo::getsFromObject($this->divisions, 'division', 'division'), 'd_id', 'multiple data-action="submits"', 'idx', 'value', (JRequest::getVar('d_id') ? JRequest::getVar('d_id') : ''), 'd_id') : JText::_('Division'));?></th>
