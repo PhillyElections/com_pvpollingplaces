899939df4216f1bca3b26fd3f544040990bfb21f
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-03-22 16:12:12 -0400

    how about all/none buttons? -- basic element inplace, no wiring

--- admin/views/places/tmpl/default.php
+++ admin/views/places/tmpl/default.php
@@ -31,14 +31,12 @@ $document->addStyleSheet('components/com_pvpollingplaces/assets/css/places.css')
 <?php
 $i = 0;
 foreach ($this->wards as $ward):
-?>
-    <a id="ward-all" data="all" class="mcs-item">[A]</a>
-    <a id="ward-none" data="none" class="mcs-item">[N]</a>
+?>    <a id="mcs-<?=$i++;?>" data="<?=$ward->ward;?>" class="mcs-item"><?=$ward->ward;?></a>
 <?php
 endforeach;
 ?>
-<a id="mcs-<?=$i++;?>" data="<?=$ward->ward;?>" class="mcs-item"><?=$ward->ward;?></a>
-<a id="mcs-<?=$i++;?>" data="<?=$ward->ward;?>" class="mcs-item"><?=$ward->ward;?></a>
+    <a id="ward-all" data="all" class="mcs-item">[A]</a>
+    <a id="ward-none" data="none" class="mcs-item">[N]</a>
                     </th>
                 </tr>
                <tr>
