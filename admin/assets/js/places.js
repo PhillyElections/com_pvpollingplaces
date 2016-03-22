3a90eac68c5a14fd7ba0f9cb718e24ca10d474ef
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-03-22 10:54:42 -0400

    can we remove that error

--- admin/assets/js/places.js
+++ admin/assets/js/places.js
@@ -3,13 +3,24 @@ var Places = (function ($) {
   return outer;
 })(jQuery);
 window.addEvent('domready', function() {
-  //Places.init();
+  Places.init();
 /*  var combos = document.querySelectorAll('[data-action="submits"]');
   for (var i=0; i<combos.length; i++) {
     combos[i].addEventListener('blur', function() {document.getElementById('adminForm').submit();});    
   }*/
 });
-jQuery(function($){
-  //you can now use $ as your jQuery object.
-  //var body = $( 'body' );
-});
+(function() {
+    function async_load(){
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = '//ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js';
+        var x = document.getElementsByTagName('script')[0];
+        x.parentNode.insertBefore(s, x);
+
+    }
+    if (window.attachEvent)
+        window.attachEvent('onload', async_load);
+    else
+        window.addEventListener('load', async_load, false);
+})();
