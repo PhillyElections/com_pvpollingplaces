93ea36ab97bafa83ff5fd577a2c4f0af1b5eefdd
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-03-21 22:46:32 -0400

    wiring up default checked

--- admin/views/places/tmpl/default.php
+++ admin/views/places/tmpl/default.php
@@ -14,7 +14,7 @@ $document->addCustomTag('<script src="components/com_pvpollingplaces/assets/js/p
                     <th colspan="14" id="filterWard" class="hiding">
 <?php
 foreach ($this->wards as $ward):
-?><label><input name="ward[]" type="checkbox" value="<?=$ward->ward;?>" <?=JRequest::getVar('ward', false) && in_array($i, JRequest::getVar('ward')) ? 'checked' : '';?> \><?=sprintf("%02d", $ward->ward);?></label>
+?><label><input name="ward[]" type="checkbox" value="<?=$ward->ward;?>" <?=JRequest::getVar('ward', false) && in_array($ward->ward, JRequest::getVar('ward')) ? 'checked' : '';?> \><?=sprintf("%02d", $ward->ward);?></label>
 <?php
 endforeach;
 ?>
@@ -24,7 +24,7 @@ endforeach;
                     <th colspan="14" id="filterDivision" class="hiding">
 <?php
 foreach ($this->divisions as $division):
-?><label><input name="division[]" type="checkbox" value="<?=$division->division_id;?>" <?=JRequest::getVar('division', false) && in_array($i, JRequest::getVar('division')) ? 'checked' : '';?> \><?=sprintf("%04d", $division->division_id);?></label>
+?><label><input name="division[]" type="checkbox" value="<?=$division->division_id;?>" <?=JRequest::getVar('division', false) && in_array($division->division_id, JRequest::getVar('division')) ? 'checked' : '';?> \><?=sprintf("%04d", $division->division_id);?></label>
 <?php
 endforeach;
 ?>
