87e772fc9ffdc2d6c0a3f193e265a8d8a8528368
Matthew Murphy <matthew.murphy2@phila.gov>
2016-03-24 08:31:13 -0400

    adding scope-related debugging

--- admin/assets/js/places.js
+++ admin/assets/js/places.js
@@ -8,13 +8,13 @@ var Places = (function($) {
             useOptionText: true, // Use text from option. Use false if you plan to use images
             hideselect: true, // Hide Original Select Control
             openmenuClass: 'mcs-open', // Toggle Open Button Class
-            openmenuText: jQuery(this).data('label'), // Text for button
+            openmenuText: $(this).data('label'), // Text for button
             openclass: 'open', // Class added to Toggle button on open
             containerClass: 'mcs-container', // Class of parent container
             itemClass: 'mcs-item', // Class of menu items
             idprefix: null, // Assign as ID to items eg 'item-' = #item-1, #item-2, #item-3...
             duration: 200, //Toggle Height duration
-            onOpen: function() {},
+            onOpen: function() {console.log(this)},
             onClose: function() {},
             onItemSelect: function() {}
         });
