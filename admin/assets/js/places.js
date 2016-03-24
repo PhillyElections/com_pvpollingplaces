b04be8f6b6105e722f4c56e6f006c582703f9f41
Matthew Murphy <matthew.murphy2@phila.gov>
2016-03-24 08:54:54 -0400

    splitting ward and division multislelect instances

--- admin/assets/js/places.js
+++ admin/assets/js/places.js
@@ -3,18 +3,33 @@ var Places = (function($) {
     var outer = [],
         inner = [];
     outer.init = function() {
-        $("#selectcontrol, #selectcontrol2").MultiColumnSelect({
+        $("#selectcontrol").MultiColumnSelect({
             multiple: true, // Single or Multiple Select- Default Single
             useOptionText: true, // Use text from option. Use false if you plan to use images
             hideselect: true, // Hide Original Select Control
             openmenuClass: 'mcs-open', // Toggle Open Button Class
-            openmenuText: $(this).parent().data('label'), // Text for button
+            openmenuText: 'Filter by Ward', // Text for button
             openclass: 'open', // Class added to Toggle button on open
             containerClass: 'mcs-container', // Class of parent container
             itemClass: 'mcs-item', // Class of menu items
             idprefix: null, // Assign as ID to items eg 'item-' = #item-1, #item-2, #item-3...
             duration: 200, //Toggle Height duration
-            onOpen: function() {console.log($(this).parent())},
+            onOpen: function() {},
+            onClose: function() {},
+            onItemSelect: function() {}
+        });
+        $("#selectcontrol2").MultiColumnSelect({
+            multiple: true, // Single or Multiple Select- Default Single
+            useOptionText: true, // Use text from option. Use false if you plan to use images
+            hideselect: true, // Hide Original Select Control
+            openmenuClass: 'mcs-open', // Toggle Open Button Class
+            openmenuText: 'Filter by Division', // Text for button
+            openclass: 'open', // Class added to Toggle button on open
+            containerClass: 'mcs-container', // Class of parent container
+            itemClass: 'mcs-item', // Class of menu items
+            idprefix: null, // Assign as ID to items eg 'item-' = #item-1, #item-2, #item-3...
+            duration: 200, //Toggle Height duration
+            onOpen: function() {},
             onClose: function() {},
             onItemSelect: function() {}
         });
