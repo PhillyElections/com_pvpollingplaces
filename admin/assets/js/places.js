6c6cc33cd4aaeb17994b8fa88d32ff45ff01391b
Matthew Murphy <matthew.murphy2@phila.gov>
2016-03-25 07:59:16 -0400

    add control buttons

--- admin/assets/js/places.js
+++ admin/assets/js/places.js
@@ -12,12 +12,14 @@ var Places = (function($) {
             openclass: 'open', // Class added to Toggle button on open
             containerClass: 'mcs-container', // Class of parent container
             itemClass: 'mcs-item', // Class of menu items
-            idprefix: null, // Assign as ID to items eg 'item-' = #item-1, #item-2, #item-3...
+            idprefix: 'wards-', // Assign as ID to items eg 'item-' = #item-1, #item-2, #item-3...
             duration: 200, //Toggle Height duration
             onOpen: function() {},
             onClose: function() {},
             onItemSelect: function() {}
         });
+        $('#selectcontrol').MultiColumnSelectAddItem('wards-all','All','');
+        $('#selectcontrol').MultiColumnSelectAddItem('wards-none','None','');
         $("#selectcontrol2").MultiColumnSelect({
             multiple: true, // Single or Multiple Select- Default Single
             useOptionText: true, // Use text from option. Use false if you plan to use images
@@ -27,12 +29,14 @@ var Places = (function($) {
             openclass: 'open', // Class added to Toggle button on open
             containerClass: 'mcs-container', // Class of parent container
             itemClass: 'mcs-item', // Class of menu items
-            idprefix: null, // Assign as ID to items eg 'item-' = #item-1, #item-2, #item-3...
+            idprefix: 'divs-', // Assign as ID to items eg 'item-' = #item-1, #item-2, #item-3...
             duration: 200, //Toggle Height duration
             onOpen: function() {},
             onClose: function() {},
             onItemSelect: function() {}
         });
+        $('#selectcontrol').MultiColumnSelectAddItem('divs-all','All','');
+        $('#selectcontrol').MultiColumnSelectAddItem('divs-none','None','');
     };
     return outer;
 })(jQuery);
