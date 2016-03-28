07e8cd0c9c1cbb8ec385b6f8bbe38eb0c143f43e
Matthew Murphy <matthew.murphy2@phila.gov>
2016-03-28 09:20:11 -0400

    lets make those multi-selects hidden from the start

--- admin/views/places/tmpl/default.php
+++ admin/views/places/tmpl/default.php
@@ -19,13 +19,13 @@ $document->addStyleSheet('components/com_pvpollingplaces/assets/css/places.css')
             <thead>
                 <tr>
                     <th colspan="14" id="selectcontrol" data-label="Filter by Wards">
-                        <?=JHTML::_('select.genericlist', PVCombo::getsFromObject($this->wards, 'ward', 'ward'), 'ward[]', 'multiple', 'idx', 'value', (JRequest::getVar('ward') ? JRequest::getVar('ward') : ''), 'ward');?></th>
+                        <?=JHTML::_('select.genericlist', PVCombo::getsFromObject($this->wards, 'ward', 'ward'), 'ward[]', 'multiple class="hidden"', 'idx', 'value', (JRequest::getVar('ward') ? JRequest::getVar('ward') : ''), 'ward');?></th>
                 </tr>
 <?php
 if (isset($this->divisions)):
 ?>                <tr>
                     <th id="selectcontrol2" colspan="14">
-                    <?=(isset($this->divisions) && count($this->divisions) ? JHTML::_('select.genericlist', PVCombo::getsFromObject($this->divisions, 'division_id', 'division_id'), 'd_id[]', 'multiple', 'idx', 'value', (JRequest::getVar('d_id') ? JRequest::getVar('d_id') : ''), 'd_id') : JText::_('Division'));?></th>
+                    <?=(isset($this->divisions) && count($this->divisions) ? JHTML::_('select.genericlist', PVCombo::getsFromObject($this->divisions, 'division_id', 'division_id'), 'd_id[]', 'multiple class="hidden"', 'idx', 'value', (JRequest::getVar('d_id') ? JRequest::getVar('d_id') : ''), 'd_id') : JText::_('Division'));?></th>
                 </tr>
 <?php
 endif;
