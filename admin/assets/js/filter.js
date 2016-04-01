dfb8be3600627e4c40c75b760640d4764ecdf3c9
Matthew Murphy <matthew.e.murphy@phila.gov>
2016-04-01 15:42:56 -0400

    sync

--- admin/assets/js/filter.js
+++ admin/assets/js/filter.js
@@ -17,27 +17,30 @@ jQuery.noConflict();
         });
     };
     inner.build = function() {
-        $selectcontrol = $("#selectcontrol");
-        $selectcontrol.MultiColumnSelect({
-            multiple: true,
-            useOptionText: true,
-            hideselect: true,
-            openmenuClass: 'mcs-open',
-            openmenuText: 'Filter by Ward',
-            openclass: 'open',
-            containerClass: 'mcs-container',
-            itemClass: 'mcs-item',
-            idprefix: 'wards-',
-            duration: 200,
-            onOpen: function() {},
-            onClose: function() {},
-            onItemSelect: function() {}
-        });
-        $('.mcs-container').append('<hr class="clear" />');
-        $selectcontrol.MultiColumnSelectAddItem('all', 'All', 'wards-');
-        $selectcontrol.MultiColumnSelectAddItem('none', 'None', 'wards-');
-        $selectcontrol.MultiColumnSelectAddItem('invert', 'Invert', 'wards-');
-        $selectcontrol.MultiColumnSelectAddItem('submit', 'Submit', 'wards-');
+        var selectcontrols = document.querySelectorAll("[data-filter]");
+        for (var i=0; i<selectcontrols.length; i++) {
+            $selectcontrol = $(selectcontrols[i]);
+            $selectcontrol.MultiColumnSelect({
+                multiple: true,
+                useOptionText: true,
+                hideselect: true,
+                openmenuClass: 'mcs-open',
+                openmenuText: $selectcontrol.data('filter'),
+                openclass: 'open',
+                containerClass: 'mcs-container',
+                itemClass: 'mcs-item',
+                idprefix: 'wards-',
+                duration: 200,
+                onOpen: function() {},
+                onClose: function() {},
+                onItemSelect: function() {}
+            });
+            $selectcontrol.children('.mcs-container').append('<hr class="clear" />');
+            $selectcontrol.MultiColumnSelectAddItem('all', 'All', 'wards-');
+            $selectcontrol.MultiColumnSelectAddItem('none', 'None', 'wards-');
+            $selectcontrol.MultiColumnSelectAddItem('invert', 'Invert', 'wards-');
+            $selectcontrol.MultiColumnSelectAddItem('submit', 'Submit', 'wards-');
+        }
     };
     outer.init = function() {
         inner.build();
