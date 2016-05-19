65a34e8d467d9344ea84baf0a6371996bc2029dc
matt murphy <mattyhead@gmail.com>
2016-05-19 12:11:33 -0400

    pasting in schema as reference

--- admin/views/place/tmpl/default.php
+++ admin/views/place/tmpl/default.php
@@ -47,3 +47,26 @@ $document->addCustomTag('<script src="components/com_pvpollingplaces/assets/js/p
     </tr>
 </table>
 </form>
+<!--
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `division_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `ward` char(2) NOT NULL DEFAULT '',
+  `division` char(2) NOT NULL DEFAULT '',
+  `pin_address` varchar(255) NOT NULL,
+  `display_address` varchar(255) DEFAULT NULL,
+  `zip_code` int(5) unsigned NOT NULL,
+  `location` varchar(255) NOT NULL DEFAULT '',
+  `building` char(1) NOT NULL DEFAULT '',
+  `parking` char(1) NOT NULL DEFAULT '',
+  `lat` decimal(10,8) NOT NULL,
+  `lng` decimal(11,8) NOT NULL,
+  `elat` decimal(10,0) NOT NULL,
+  `elng` decimal(11,0) NOT NULL,
+  `alat` decimal(10,0) NOT NULL,
+  `alng` decimal(11,0) NOT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT '0',
+  `checked_out` int(10) NOT NULL DEFAULT '0',
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+-->
