10e030037dc3c5aae1741fb15efd210cdce8582c
matthew murphy <mattyhead@gmail.com>
2016-05-21 18:30:56 -0400

    declaring explicitly

--- admin/assets/js/place.js
+++ admin/assets/js/place.js
@@ -1,8 +1,9 @@
 var place = (function(d) {
   'use strict';
   var inner = {}, outer = {};
+  inner.markers = inner.location = {};
   inner.markers.building = inner.markers.entrance = inner.markers.accessible = inner.listener = null;
-  inner.location = {};
+  
   inner.apikey = 'AIzaSyDG7jgg6RbsEKG7UFXsSPi7F5RyRDTasnE';
   //key='+inner.apikey+'
   // hot init function
