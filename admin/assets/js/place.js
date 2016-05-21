8287e77b7b725f3089c9170dffc6d54775fe1b6e
matthew murphy <mattyhead@gmail.com>
2016-05-21 16:17:08 -0400

    Adding some event code to steal

--- admin/assets/js/place.js
+++ admin/assets/js/place.js
@@ -1,10 +1,10 @@
 var place = (function(d) {
   'use strict';
-  var inner = {}, outer = {};
-  inner.location={};
+  var inner = {},
+    outer = {};
+  inner.location = {};
   inner.apikey = 'AIzaSyDG7jgg6RbsEKG7UFXsSPi7F5RyRDTasnE';
-//key='+inner.apikey+'
-
+  //key='+inner.apikey+'
   // hot init function
   outer.init = function() {
     var script = d.createElement('script');
@@ -13,19 +13,16 @@ var place = (function(d) {
     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&callback=place.mapDisplay';
     d.body.appendChild(script);
   };
-
-  outer.mapDisplay = function () {
+  outer.mapDisplay = function() {
     inner.setLocations();
-
     inner.map = new google.maps.Map(d.getElementById('map'), {
       center: inner.location.building,
-      zoom: 19
+      zoom: 19,
+      navigationControl: false
     });
-
     outer.markerDisplay();
   };
-
-  outer.markerDisplay = function () {
+  outer.markerDisplay = function() {
     var buildingMarker = new google.maps.Marker({
       position: inner.location.building,
       map: inner.map,
@@ -33,26 +30,66 @@ var place = (function(d) {
       icon: 'components/com_pvpollingplaces/assets/images/b.png'
     });
   };
-
-  inner.setLocations = function () {
+  inner.setLocations = function() {
     inner.locationName = d.getElementById('location').value;
     inner.location.building = {
       lat: parseFloat(d.getElementById('lat').value),
       lng: parseFloat(d.getElementById('lng').value)
     };
-
     inner.location.entrance = {
       lat: parseFloat(d.getElementById('elat').value),
       lng: parseFloat(d.getElementById('elng').value)
     };
-
     inner.location.accessible = {
       lat: parseFloat(d.getElementById('alat').value),
       lng: parseFloat(d.getElementById('alng').value)
     };
     console.log(inner.location);
   }
+  var infowindow = new google.maps.InfoWindow({
+    size: new google.maps.Size(150, 50)
+  });
+  // A function to create the marker and set up the event window function
+  function createMarker(latlng, name, html) {
+    var contentString = html;
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: map,
+      zIndex: Math.round(latlng.lat() * -100000) << 5
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(contentString);
+      infowindow.open(map, marker);
+    });
+    google.maps.event.trigger(marker, 'click');
+    return marker;
+  }
 
+  function initialize() {
+    // create the map
+    var myOptions = {
+      zoom: 8,
+      center: new google.maps.LatLng(43.907787, -79.359741),
+      mapTypeControl: true,
+      mapTypeControlOptions: { style: google.maps.MapTypeControlStyle.DROPDOWN_MENU },
+      navigationControl: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    map = new google.maps.Map(document.getElementById("map_canvas"),
+      myOptions);
+
+    google.maps.event.addListener(map, 'click', function() {
+      infowindow.close();
+    });
+    google.maps.event.addListener(map, 'click', function(event) {
+      //call function to create marker
+      if (marker) {
+        marker.setMap(null);
+        marker = null;
+      }
+      marker = createMarker(event.latLng, "name", "<b>Location</b><br>" + event.latLng);
+    });
+  }
   return outer;
 })(document);
 window.addEvent('domready', function() {
