18c7dadc7f52411ae0d16db256f6386d4c61b366
matthew murphy <mattyhead@gmail.com>
2016-05-22 17:45:31 -0400

    Dropping debugg, rewiring listeners

--- admin/assets/js/place.js
+++ admin/assets/js/place.js
@@ -63,10 +63,8 @@ var place = (function(d) {
 
     inner.setElements();
     inner.setLocations();
-    console.log(markers);
-    for (var i = 0; i < 3; i++) {
-      console.log(markers[i]);
-      markers[i].addListener('click', function() { /*inner.addListener(markers[i].dataset.marker)*/ alert('alert! ' + this.dataset.marker) });
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].addListener('click', function() { inner.addListener(this.dataset.marker); });
     }
 
   };
