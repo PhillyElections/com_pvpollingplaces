77513172638f7119b60ed9e759e1088a715f186f
matthew murphy <mattyhead@gmail.com>
2016-05-22 15:44:38 -0400

    Dropping some example code

--- admin/assets/js/place.js
+++ admin/assets/js/place.js
@@ -92,52 +92,7 @@ var place = (function(d) {
       inner.elements[type].lng.value = event.latLng.lng();
     });
   };
-  /*
-    var infowindow = new google.maps.InfoWindow({
-      size: new google.maps.Size(150, 50)
-    });
-    // A function to create the marker and set up the event window function
-    function createMarker(latlng, name, html) {
-      var contentString = html;
-      var marker = new google.maps.Marker({
-        position: latlng,
-        map: map,
-        zIndex: Math.round(latlng.lat() * -100000) << 5
-      });
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(contentString);
-        infowindow.open(map, marker);
-      });
-      google.maps.event.trigger(marker, 'click');
-      return marker;
-    }
 
-    function initialize() {
-      // create the map
-      var myOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(43.907787, -79.359741),
-        mapTypeControl: true,
-        mapTypeControlOptions: { style: google.maps.MapTypeControlStyle.DROPDOWN_MENU },
-        navigationControl: true,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-      map = new google.maps.Map(document.getElementById("map_canvas"),
-        myOptions);
-
-      google.maps.event.addListener(map, 'click', function() {
-        infowindow.close();
-      });
-      google.maps.event.addListener(map, 'click', function(event) {
-        //call function to create marker
-        if (marker) {
-          marker.setMap(null);
-          marker = null;
-        }
-        marker = createMarker(event.latLng, "name", "<b>Location</b><br>" + event.latLng);
-      });
-    }
-  */
   return outer;
 })(document);
 window.addEvent('domready', function() {
