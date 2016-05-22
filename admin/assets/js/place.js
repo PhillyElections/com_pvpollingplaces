7c2e302053c2c62a241f7511d849873d8d0e9aca
matthew murphy <mattyhead@gmail.com>
2016-05-22 16:20:31 -0400

    Let's examine those looped elements again...

--- admin/assets/js/place.js
+++ admin/assets/js/place.js
@@ -63,8 +63,8 @@ var place = (function(d) {
     inner.setElements();
     inner.setLocations();
     for (var marker in d.querySelectorAll("img.marker")) {
-      console.log(marker.dataset);
-      marker.addListener('click', function() { inner.addListener(marker.dataset.marker) });
+      console.log(marker);
+      //marker.addListener('click', function() { inner.addListener(marker.dataset.marker) });
     }
 
   };
