ad1c225d38e9918de460f1b1573ca351894835a7
matthew murphy <mattyhead@gmail.com>
2016-05-23 14:54:18 -0400

    Dropping debug dump again

--- admin/models/place.php
+++ admin/models/place.php
@@ -107,7 +107,7 @@ class PvpollingplacesModelPlace extends JModel
             }
             return false;
         }
-        dd($row, $data);
+
         // Store the web link table to the database
         if (!$row->store()) {
             $this->setError($row->getErrorMsg());
