8c1cd996da6c0f8fa9c80449c20833764dec4e28
Matthew Murphy <mattyhead@gmail.com>
2016-05-25 14:19:53 -0400

    Replacing strings with constants

--- admin/views/places/tmpl/default.php
+++ admin/views/places/tmpl/default.php
@@ -1,7 +1,7 @@
 <?php defined('_JEXEC') or die('Restricted access');
 $pagination = &$this->pagination;
 
-jimport("pvcombo.PVCombo");
+jimport('pvcombo.PVCombo');
 
 $document = &JFactory::getDocument();
 $document->addCustomTag('<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>');
@@ -13,71 +13,78 @@ $document->addStyleSheet('components/com_pvpollingplaces/assets/css/filter.css')
 <form action="<?=JRoute::_('index.php?option=com_pvpollingplaces');?>" method="post" name="adminForm" id="adminForm">
     <div id="editcell">
         <table class="adminlist">
+        <thead>
 <?php
-if (count($this->items) or JRequest::getVar('ward')):
+if (count($this->items) or JRequest::getVar('ward')) :
 ?>
-                <tr>
-                    <th colspan="14" id="selectcontrol" data-filter="Filter by Wards">
-                        <?=JHTML::_('select.genericlist', PVCombo::getsFromObject($this->wards, 'ward', 'ward'), 'ward[]', 'multiple', 'idx', 'value', (JRequest::getVar('ward') ? JRequest::getVar('ward') : ''), 'ward');?></th>
-                </tr>
+            <tr>
+                <th colspan="14" id="selectcontrol" data-filter="Filter by Wards">
+                    <?=JHTML::_('select.genericlist', PVCombo::getsFromObject($this->wards, 'ward', 'ward'), 'ward[]', 'multiple', 'idx', 'value', (JRequest::getVar('ward') ? JRequest::getVar('ward') : ''), 'ward');?></th>
+            </tr>
 <?php
 endif;
 ?>
-                <tr>
-                    <th width="5px">
-                        <?=JText::_('ID');?>
-                    </th>
-                    <th width="5px">
-                        <input type="checkbox" name="toggle" value="" onclick="checkAll(<?=count($this->items);?>);" />
-                    </th>
-                    <th width="5px">
-                        P
-                    </th>
-                    <th><?=JText::_('Ward');?></th>
-                    <th><?=JText::_('Division');?></th>
-                    <th><?=JText::_('Pin');?></th>
-                    <th><?=JText::_('Display');?></th>
-                    <th><?=JText::_('Name');?></th>
-                    <th><?=JText::_('Location');?></th>
-                    <th><?=JText::_('Entrance');?></th>
-                    <th><?=JText::_('Accessiblility');?></th>
-                    <th><?=JText::_('Published');?></th>
-                    <th><?=JText::_('Created');?></th>
-                    <th><?=JText::_('Updated');?></th>
-                </tr>
-            </thead>
+            <tr>
+                <th width="5px">
+                    <?=JText::_('ID');?>
+                </th>
+                <th width="5px">
+                    <input type="checkbox" name="toggle" value="" onclick="checkAll(<?=count($this->items);?>);" />
+                </th>
+                <th width="5px">
+                    P
+                </th>
+                <th><?=JText::_('WARD');?></th>
+                <th><?=JText::_('DIVISION');?></th>
+                <th><?=JText::_('PIN');?></th>
+                <th><?=JText::_('DISPLAY');?></th>
+                <th><?=JText::_('NAME');?></th>
+                <th><?=JText::_('LOCATION');?></th>
+                <th><?=JText::_('ENTRANCE');?></th>
+                <th><?=JText::_('ACCESSIBLILITY');?></th>
+                <th><?=JText::_('PUBLISHED');?></th>
+                <th><?=JText::_('CREATED');?></th>
+                <th><?=JText::_('UPDATED');?></th>
+            </tr>
+        </thead>
+        <tbody>
             <?php
-$k = 0;
-for ($i = 0, $n = count($this->items); $i < $n; $i++) {
-    $row = &$this->items[$i];
-    $link = JRoute::_('index.php?option=com_pvpollingplaces&controller=place&task=edit&cid[]=' . $row->id);
-    ?>
-            <tr class="<?="row$k";?>">
+            $k = 0;
+            for ($i = 0, $n = count($this->items); $i < $n; ++$i) {
+                $row = &$this->items[$i];
+                $link = JRoute::_('index.php?option=com_pvpollingplaces&controller=place&task=edit&cid[]='.$row->id);
+                ?>
+                        <tr class="<?="row$k";
+                ?>">
                 <td>
-                    <?=$row->id;?>
+                    <?=$row->id;
+                ?>
                 </td>
-            <td>
-                <?=JHTML::_('grid.id', $i, $row->id);?>
-            </td>
-            <td>
-                <?=JHTML::_('grid.published', $row, $i);?>
-            </td>
+                        <td>
+                <?=JHTML::_('grid.id', $i, $row->id);
+                ?>
+                        </td>
+                        <td>
+                <?=JHTML::_('grid.published', $row, $i);
+                ?>
+                        </td>
                 <td><?=$row->ward;?></td>
                 <td><?=$row->division;?></td>
                 <td><?=$row->pin_address;?></td>
                 <td><?=$row->display_address;?></td>
-                <td><a href="<?=$link;?>"><?=$row->location;?></a></td>
-                <td><?=$row->lat;?>,<?=$row->lng;?></td>
-                <td><?=$row->elat;?>,<?=$row->elng;?></td>
-                <td><?=$row->alat;?>,<?=$row->alng;?></td>
+                <td><a href="<?=$link?>"><?=$row->location;?></a></td>
+                <td><?=$row->lat?>,<?=$row->lng;?></td>
+                <td><?=$row->elat?>,<?=$row->elng;?></td>
+                <td><?=$row->alat?>,<?=$row->alng;?></td>
                 <td><?=$row->published;?></td>
                 <td><?=$row->created;?></td>
                 <td><?=$row->updated;?></td>
             </tr>
             <?php
-$k = 1 - $k;
-}
+            $k = 1 - $k;
+            }
 ?>
+            </tbody>
             <tfoot>
             <tr>
                 <td colspan="14"><?php echo $this->pagination->getListFooter(); ?></td>
