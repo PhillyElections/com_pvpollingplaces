5dd68150a23ec54f17cb5b27a0daac77bce27ee7
Matthew Murphy <mattyhead@gmail.com>
2017-01-15 20:07:47 -0500

    In the middle of populating the xref

--- admin/sql/install.sql
+++ admin/sql/install.sql
@@ -28,12 +28,24 @@ CREATE TABLE IF NOT EXISTS `#__pv_pollingplaces` (
   `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ;
+) ENGINE=ARIA DEFAULT CHARSET=utf8 ;
+
+CREATE TABLE IF NOT EXISTS `#__pv_pollingplace_divisions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `pollingplace_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `division_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `published` tinyint(1) NOT NULL DEFAULT '0',
+  `checked_out` int(10) NOT NULL DEFAULT '0',
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=ARIA DEFAULT CHARSET=utf8 ;
 
 INSERT INTO `#__pv_pollingplaces` 
   (`pin_address`, `location`, `published`, `created`) 
-SELECT DISTINCT `pin_address`, `location`, 1, @tnl FROM `#__pollingplaces`;
- 
+SELECT DISTINCT `pin_address`, `location`, 1, @tnow FROM `#__pollingplaces`;
+
 UPDATE `#__pv_pollingplaces` as `p` 
 SET 
   `p`.`display_address`=(SELECT `display_address` FROM `#__pollingplaces` where `pin_address`=`p`.`pin_address` limit 1),
@@ -47,3 +59,7 @@ SET
   `p`.`elng`=(SELECT max(`elng`) FROM `#__pollingplaces` where `pin_address`=`p`.`pin_address`),
   `p`.`alat`=(SELECT min(`alng`) FROM `#__pollingplaces` where `pin_address`=`p`.`pin_address`),
   `p`.`alng`=(SELECT max(`alng`) FROM `#__pollingplaces` where `pin_address`=`p`.`pin_address`);
+
+INSERT INTO `#__pv_pollingplace_divisions`
+  (`pollingplace_id`, `division_id`, `published`, `created`) 
+SELECT `p`.`id`, `d`.`id`, 1, @tnow
